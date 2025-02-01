f9531b3e8a74545dda601e445a5acc74f7e4cb16
JavierBuenoPatience <javibueda@gmail.com>
2025-02-01 13:47:24 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./patience.db"
+# Se usa DATABASE_URL si está definida en el entorno; de lo contrario, se usa SQLite.
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./patience.db")
 
 engine = create_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-print("sqlite:///./patience.db", engine.url)
+print("Database URL:", engine.url)
