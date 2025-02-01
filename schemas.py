c2ad67ec94afcfaf44a8de9e73365815d12c8c94
JavierBuenoPatience <javibueda@gmail.com>
2025-02-01 13:47:58 +0100

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -8,6 +8,13 @@ class UserCreate(BaseModel):
     class Config:
         from_attributes = True
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        from_attributes = True
+
 class UserResponse(BaseModel):
     id: int
     name: str
