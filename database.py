179c94c2fb57f136ffba682dc877067831689e2c
JavierBuenoPatience <javibueda@gmail.com>
2025-02-09 14:52:43 +0100

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Se usa DATABASE_URL si está definida en el entorno; de lo contrario, se usa SQLite.
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./patience.db")
 
 engine = create_engine(DATABASE_URL, echo=True)
