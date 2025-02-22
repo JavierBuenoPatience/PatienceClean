833efeec9d489bb172d7be8f7db2003784e73d8d
JavierBuenoPatience <javibueda@gmail.com>
2025-02-22 09:06:23 +0100

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,12 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./patience.db")
 
-engine = create_engine(DATABASE_URL, echo=True)
+# Para SQLite, es necesario desactivar la restricción de hilo único
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL, echo=True)
+
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
